f63faa4ee26d2b84d0d0e1482c0dd7b566482e57
GameDesert <GameDesert@users.noreply.github.com>
2021-01-27 12:00:40 +0000

    added cookie banner

---
+++ cookie_banner.js
@@ -0,0 +1,7 @@
+if (localStorage.getItem('cookieSeen') != 'shown') {
+  $('.cookie-banner').delay(2000).fadeIn();
+  localStorage.setItem('cookieSeen','shown')
+};
+$('.close').click(function() {
+  $('.cookie-banner').fadeOut();
+})
