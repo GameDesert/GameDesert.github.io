cd474d0f664d797ca1f0f0cba9529f7df1bc4587
GameDesert <GameDesert@users.noreply.github.com>
2021-01-27 10:33:03 +0000

    added subdomain redirects

---
+++ index_script.js
@@ -0,0 +1,13 @@
+//Redirect Subdomains to Directory
+
+var URL = window.location.host
+console.log("Redirect from: " + URL);
+
+if (URL = "pawel.kotla.eu") {
+window.location.replace("https://en.wikipedia.org/wiki/Paweł_Kotla");
+}
+/*
+else if (URL = "[Another Input URL]") {
+window.location.replace("[Another Redirect URL]");
+}
+*/
