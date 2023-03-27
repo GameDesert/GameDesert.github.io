6611a1a4a1174e8640ee98ec634c30117d152019
GameDesert <56128805+GameDesert@users.noreply.github.com>
2023-03-27 20:01:28 +0100

    Added some accessibility stuff

--- new-index/scripts/index.js
+++ new-index/scripts/index.js
@@ -2,4 +2,14 @@ Mousetrap.bind('up up down down left right left right b a enter', function() {
     alert("Hello world!")
 });
 
-AOS.init();
+AOS.init({
+    disable: window.matchMedia('(prefers-reduced-motion: reduce)')["matches"]
+});
+
+function disableTilt() {
+    var tiltable = document.querySelectorAll(".tiltable");
+    for (var i = 0; i < tiltable.length; i++) {
+        VanillaTilt.init(tiltable[i])
+        tiltable[i].vanillaTilt.destroy();
+    }
+}
